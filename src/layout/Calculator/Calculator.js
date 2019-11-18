99bd1cea892a93cd1e9e346bbc253ae667d0da75
Jackson Newlands <jacksonn@stratinfotech.com>
2019-11-18 11:00:02 -0500

    Increased functionality of calculator. Made it more user friendly

--- src/layout/Calculator/Calculator.js
+++ src/layout/Calculator/Calculator.js
@@ -13,13 +13,23 @@ class Calculator extends React.Component {
     let equation = this.state.equation;
     const pressedButton = event.target.innerHTML;
     if (pressedButton === 'C') return this.clear();
-    else if ((pressedButton >= '0' && pressedButton <= '9') || pressedButton === '.') equation += pressedButton;
-    else if (['+', '-', '*', '/', '%'].indexOf(pressedButton) !== -1) equation += ' ' + pressedButton + ' ';
+    else if ((pressedButton >= '0' && pressedButton <= '9') || pressedButton === '.') {
+		if (this.state.equation === '') this.setState({result: 0});  /* If a number is clicked and equation is empty, reset results */
+		if (pressedButton === '.' && this.state.equation === '') equation += '0';
+		equation += pressedButton;
+	}
+    else if (['+', '-', '*', '/', '%'].indexOf(pressedButton) !== -1) {
+		if (this.state.equation === '')
+			equation += this.state.result + ' ' + pressedButton + ' ';
+		else equation += ' ' + pressedButton + ' ';
+	}
     else if (pressedButton === '=') {
       try {
         const evalResult = eval(equation);
         const result = Number.isInteger(evalResult) ? evalResult : evalResult.toFixed(2);
+		equation = '';
         this.setState({result});
+		this.setState({equation});
       } catch (error) {
         alert('Invalid Mathematical Equation');
 		return this.clear();
@@ -30,10 +40,9 @@ class Calculator extends React.Component {
       equation = equation.trim();
       equation = equation.substr(0, equation.length - 1);
     }
-                
     this.setState({equation: equation});
   }
-  
+ 
   clear() {
     this.setState({equation: '', result: 0});
   }
